3a233bd0862984a68c15d6d4876af2047a341d49
KatePC <moeriac@gmail.com>
2026-02-15 17:30:44 +0300

    Создал страницу index.html всех людей

--- src/main/java/ru/moerti/springprojects/controllers/PeopleController.java
+++ src/main/java/ru/moerti/springprojects/controllers/PeopleController.java
@@ -1,6 +1,9 @@
 package ru.moerti.springprojects.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.moerti.springprojects.dao.PersonDAO;
 import ru.moerti.springprojects.util.PersonValidator;
@@ -11,4 +14,17 @@ public class PeopleController {
 
     private final PersonDAO personDAO;
     private final PersonValidator personValidator;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
+        this.personDAO = personDAO;
+        this.personValidator = personValidator;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("people", personDAO.index());
+        return "people/show";
+    }
+
 }
